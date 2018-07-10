8877ec974694b026ca51837dfc7d6c5b4c4caa4c
justi <justi@192.168.254.3>
2018-07-09 22:12:31 -0400

    String stream example

---
+++ String/String.cpp
@@ -0,0 +1,51 @@
+/*
+ * String.cpp
+ *
+ *  Created on: Jul 7, 2018
+ *      Author: justi
+ */
+
+#include <iostream>
+#include<sstream>
+using namespace std;
+
+int main() {
+
+	// object (string is a class)
+	string value = "Hello this is a string. ";
+	string value2 = "How are you doing?";
+	string greeting = "Welcome to my C++ code";
+
+	string val3 = value + value2;
+
+	cout << value << value2 << endl;
+	cout << "concatenation: " << val3 << endl;
+	cout << greeting << endl;
+
+	cout << "What's your name?" << endl;
+	string input; // declare an object of type string
+
+	cin >> input;
+	cout << "Hello, " << input << " How much EXP do you have in C++? " << flush;
+	int exp;
+	cin >> exp;
+	cout << exp << " experience points?" << endl;
+	//insertion operator: <<
+	//extraction operator: >>
+
+	string name = input;
+	int age = 30;
+
+	stringstream ss;
+
+	ss << "Name  is: ";
+	ss << name;
+	ss << "Age  is: ";
+	ss << age;
+
+	string info = ss.str();
+
+	cout << info << endl;
+
+	return 0;
+}
